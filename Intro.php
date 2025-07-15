0cc96b4a09581c75e8d66295e7618a62ff718ba1
Andrew Chakwera <chakweraandrew@gmail.com>
2025-07-15 14:33:12 +0200

    second day

--- Intro.php
+++ Intro.php
@@ -12,24 +12,14 @@ $students= array ("mateyu", "Philipo", "Moses", "John", "jane");
     <title>Laravel project</title>
 </head>
 <body>
-    <h1>About User</h1>
-    <p>
-    <?php
-    if ($marks>=80) {echo "distinction <br>";}
-     elseif($marks>=70)
-      {echo "This is a credit";}
-      else {echo "failed";}
-    foreach($students as $index =>$name)  
-    {echo "students $index: $name <br>";}
-    echo "<table border='1'>";
-        echo "<tr><th>Index</th><th>Name</th></tr>";
-        foreach ($students as $key => $val)
-        {
-            echo "<tr><td>" . ($key+1) . "</td> <td>. $val .</td></tr>";
-        }
-        echo "</table>";
-        ?>
+<form action="submit.php" method="post">
+        <label for="intro"> Personal Information;</label><br>
+        <input type="text" name="username" required><br>
+        <input type="number" name="age" required><br>
+        <input type="email" name="email" required><br>
+        <button type="submit">Submit</button><br>
      
      </p>
 </body>
+
 </html>
